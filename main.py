937e6bb6b9c990f3da57e8bd17bd632cc2e2e553
Juli <laterzajulian@gmail.com>
2023-04-30 16:26:48 -0300

    Login and Verify Login

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 """
 Open page
 Type username student into Username field
@@ -17,43 +15,44 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-#Acá se crea el driver de Chrome. La instancia de Navegador.
+# Acá se crea el driver de Chrome. La instancia de Navegador.
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-#Abrimos la página
+# Abrimos la página
 driver.get("https://practicetestautomation.com/practice-test-login/")
 driver.maximize_window()
 time.sleep(3)
 
-#inputs = driver.find_elements(by=By.TAG_NAME,value="input")
-#userInput = inputs[0]
+# inputs = driver.find_elements(by=By.TAG_NAME,value="input")
+# userInput = inputs[0]
 
-#Type Username Student
+# Type Username Student
 userInput = driver.find_element(by=By.ID, value="username")
 userInput.send_keys('student')
 
-#Type Password Student
+# Type Password Student
 passwordInput = driver.find_element(by=By.ID, value="password")
 passwordInput.send_keys("Password123")
 
-#Clic Submit Button.
+# Clic Submit Button.
 subBtn = driver.find_element(by=By.XPATH, value='//*[@id="submit"]')
 subBtn.click()
 time.sleep(4)
 
-
-#Verificar si la URL cambió luego de loguearse.
+# Verificar si la URL cambió luego de loguearse.
 
 actualURL = driver.current_url
+assert actualURL == "https://practicetestautomation.com/logged-in-successfully/"
 
-#Text locator
-text_locator = driver.find_element(By.TAG_NAME,value='h1')
+# Text locator
+text_locator = driver.find_element(By.TAG_NAME, value='h1')
 actual_text = text_locator.text
 
+assert actual_text == "Logged In Successfully"
 
-#logOut locator
-log_outBtn = driver.find_element(By.LINK_TEXT,value="Log out")
+# logOut locator
+log_outBtn = driver.find_element(By.LINK_TEXT, value="Log out")
+assert log_outBtn.is_displayed()
 
-
-print("Se cierra el navegador.")
+print("Ejecución Exitosa")
 driver.quit()
